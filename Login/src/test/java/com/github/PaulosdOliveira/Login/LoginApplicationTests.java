39ea16656b459d885dd7544fdab84ebd98423811
PaulosdOliveira <pauloricardomilos@gmail.com>
2025-04-04 20:48:28 -0300

    Criação do serviço de login

---
+++ Login/src/test/java/com/github/PaulosdOliveira/Login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.PaulosdOliveira.Login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
